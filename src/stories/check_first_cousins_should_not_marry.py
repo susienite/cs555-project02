1f508965fa611f29cb884eb50e245a9769313e35
Ryan-Hu13 <98501065+Ryan-Hu13@users.noreply.github.com>
2023-08-10 23:51:21 -0400

    finished first cousins shouldn't marry
    code finally works ;-;

--- src/stories/check_first_cousins_should_not_marry.py
+++ src/stories/check_first_cousins_should_not_marry.py
@@ -1,14 +1,31 @@
 from helper import *
 
-def check_no_bigamy(fam_data, anomalies):
-    married = {}
-    for fam in fam_data:
-        if(fam['HusbandId'] not in married.keys() and fam['Divorced'] == 'NA'):
-            married.update({fam['HusbandId']: fam['ID']})
-        elif(fam['Divorced'] == 'NA'):
-            anomalies.append(f'ANOMALY: FAMILY: US11: {fam["HusbandName"]} ({fam["HusbandId"]}) has a bigamous marriage')
+def check_first_cousins_should_not_marry(fam_data, anomalies):
 
-        if(fam['WifeId'] not in married.keys() and fam['Divorced'] == 'NA'):
-            married.update({fam['WifeId']: fam['ID']})
-        elif(fam['Divorced'] == 'NA'):
-            anomalies.append(f'ANOMALY: FAMILY: US11: {fam["WifeName"]} ({fam["WifeId"]}) has a bigamous marriage')
+    # Get a mapping of parents to their children
+    children_of_parents = {}
+    for fam in fam_data:
+        children_of_parents[(fam.get('HusbandId'), fam.get('WifeId'))] = fam.get('Children', [])
+
+    # Check if any married couples are first cousins
+    for (parents1, children1) in children_of_parents.items():
+        for (parents2, children2) in children_of_parents.items():
+            if parents1 != parents2:  # Ensure we're not comparing the same family
+                for child1 in children1:
+                    for child2 in children2:
+                        if are_married(child1, child2, fam_data):
+                            anomalies.append(f'ANOMALY: FAMILY: US19: {get_name(child1, fam_data)} ({child1}) and {get_name(child2, fam_data)} ({child2}) are first cousins and should not be married.')
+
+def are_married(individual1, individual2, fam_data):
+    for fam in fam_data:
+        if (fam.get('HusbandId') == individual1 and fam.get('WifeId') == individual2) or (fam.get('HusbandId') == individual2 and fam.get('WifeId') == individual1):
+            return True
+    return False
+
+def get_name(individual_id, fam_data):
+    for fam in fam_data:
+        if fam.get('HusbandId') == individual_id:
+            return fam.get('HusbandName')
+        if fam.get('WifeId') == individual_id:
+            return fam.get('WifeName')
+    return None
--- src/tests/test_first_cousin_marriage.py
+++ src/tests/test_first_cousin_marriage.py
@@ -1,51 +1,34 @@
 import unittest
 
-f_no_error = "./test_geds/Issac-fam.ged"
-f_errors = "./test_geds/first-cousins-are-married.ged"
+# Test files
+f_no_error = "./test_geds/my-fam.ged"
+f_errors_cousins = "./test_geds/first-cousins-are-married.ged"
 
-#make src/ visible for import
 import sys
 sys.path.append("../")
 
 from A3parser import parser
-from stories.check_first_cousins_should_not_marry import list_married_living
+from stories.check_first_cousins_should_not_marry import check_first_cousins_should_not_marry 
 
-#test cases here.
+class TestCousinMarriage(unittest.TestCase):
 
-class TestMarriedLiving(unittest.TestCase):
     def prep(self, filepath): 
-        self.indi_data, self.fam_data = parser(filepath)   
-    
+        self.indi_data, self.fam_data = parser(filepath)
+        self.errors = []    
+        self.anomalies = []
+
+    def tearDown(self):  # Reset errors after each test
+        self.anomalies = []
+
     def test_no_errors(self):
         self.prep(f_no_error)
-        list1 = list_married_living(self.indi_data, self.fam_data)
-        self.assertEqual(len(list1)-1, 0)  
+        check_first_cousins_should_not_marry(self.fam_data, self.anomalies)
+        self.assertEqual(len(self.anomalies), 0)
 
     def test_errors(self):
-        self.prep(f_errors)
-        list1 = list_married_living(self.indi_data, self.fam_data)
-        self.assertEqual(len(list1)-1, 4) 
-
-    def test_headings(self):
-        self.prep(f_errors)
-        list1 = list_married_living(self.indi_data, self.fam_data)
-        self.assertEqual(list1[0], "List of Married Living Individuals:")
-
-    def test_error_1(self):
-        self.prep(f_errors)
-        list1 = list_married_living(self.indi_data, self.fam_data)
-        self.assertEqual(list1[1], "\tZach /Tan/ and Winnie /Chen/ are married and living")
-
-    def test_error_2(self):
-        self.prep(f_errors)
-        list1 = list_married_living(self.indi_data, self.fam_data)
-        self.assertEqual(list1[2], "\tSam /Chen/ and Julia /Kim/ are married and living")
-        
-    def test_error_3(self):
-        self.prep(f_errors)
-        list1 = list_married_living(self.indi_data, self.fam_data)
-        self.assertEqual(list1[3], "\tDavid /Li/ and Bonnie /Chen/ are married and living")
-        
+        self.prep(f_errors_cousins)
+        check_first_cousins_should_not_marry(self.fam_data, self.anomalies)
+        self.assertEqual(self.anomalies[0], "ANOMALY: FAMILY: US19: Dani /Tan/ (I1) and Bonnie /Chen/ (I7) are first cousins and should not be married.")
 
 if __name__ == '__main__':
     unittest.main()
